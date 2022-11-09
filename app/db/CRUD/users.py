54671950faab241a2e618ad379f9de4acd5636a0
samolin <samolin.vlad@gmail.com>
2022-11-09 23:23:20 +0300

    add hash function for password

--- app/db/CRUD/users.py
+++ app/db/CRUD/users.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from app.schemas.users import UserCreate
-
+from app.core.hashing import Hasher
 from ..models.users import User
 
 
@@ -14,7 +14,7 @@ def create_new_user(user: UserCreate, db: Session):
     user = User(
         username=user.username,
         email=user.email,
-        hashed_password=user.password,
+        hashed_password=Hasher.get_password_hash(user.password),
         is_active=True,
         is_superuser=False,
     )
